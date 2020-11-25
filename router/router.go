d7cbd97c399da9d14837dc8de94605aab6a79bd4
ShuwenHe <1201220707@pku.edu.cn>
2020-11-25 10:18:55 +0800

    feat: order

--- router/router.go
+++ router/router.go
@@ -11,44 +11,10 @@ func Run() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("views/static"))))
 	http.Handle("/pages/", http.StripPrefix("/pages/", http.FileServer(http.Dir("views/pages"))))
 
-	User()
-	Item()
-	Cart()
-	CartItem()
 	Order()
 
 	port := viper.GetString("server.port")
 	http.ListenAndServe(port, nil) // Listen and serve
-}
-
-// UserRouter User router
-func User() {
-	http.HandleFunc("/login", controller.Login)
-	http.HandleFunc("/logout", controller.Logout)
-	http.HandleFunc("/regist", controller.Regist)
-	http.HandleFunc("/checkUserName", controller.CheckUserName)
-}
-
-func Item()  {
-	http.HandleFunc("/", controller.GetPageBooksByPrice)
-	http.HandleFunc("/getPageBooks", controller.GetPageBooks)
-	http.HandleFunc("/getPageBooksByPrice", controller.GetPageBooksByPrice)
-	http.HandleFunc("/deleteBook", controller.DeleteBook)
-	http.HandleFunc("/toUpdateBookPage", controller.ToUpdateBookPage) // Go to the page to update the book
-	http.HandleFunc("/updateOraddBook", controller.UpdateOrAddBook)   // Update or add books
-	http.HandleFunc("/addBook2Cart", controller.AddBook2Cart)         // Add books to cart
-}
-
-// CartRouter Cart router
-func Cart() {
-	http.HandleFunc("/getCartInfo", controller.GetCartInfo) // Get cart information
-	http.HandleFunc("/deleteCart", controller.DeleteCart)
-}
-
-// CartItemRouter Cart item router
-func CartItem() {
-	http.HandleFunc("/deleteCartItem", controller.DeleteCartItem)
-	http.HandleFunc("/updateCartItem", controller.UpdateCartItem) // updateCartItem
 }
 
 func Order()  {
